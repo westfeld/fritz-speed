fc47d860531b30b00da0b704dd8384c23b500c3d
Thomas Westfeld <westfeld@mac.com>
2016-02-01 21:07:08 +0100

    adapted monitoring script to use configuration file

--- monitor-traffic.py
+++ monitor-traffic.py
@@ -3,16 +3,19 @@
 
 """
 traffic-monitor.py
+
+querys the Fritz!Box via the TR-064 interface and writes traffic counters
+into round robin archive (RRA).
 """
 
 import fritzconnection
 import rrdtool
 
-rrd_file = "/var/rrdtool/wan_traffic.rra"
+from common import read_configuration
 
-def update_rra (*args):
+def update_rra (rra_filename, *args):
     data = ":".join(args)
-    rrdtool.update(rrd_file, "N:"+data)
+    rrdtool.update(rra_filename, "N:"+data)
 
 def main():
     fc = fritzconnection.FritzConnection()
@@ -21,7 +24,8 @@ def main():
 
     status = fc.call_action('WANCommonInterfaceConfig', 'GetTotalBytesReceived')
     bytes_down =  status['NewTotalBytesReceived']
-    update_rra(str(bytes_up), str(bytes_down))
+    update_rra(prefs['rra_filename'], str(bytes_up), str(bytes_down))
 
 if __name__ == '__main__':
+    prefs = read_configuration('fritz-speed.ini')
     main()
